85f9293da490b851462a1429990bdf58dac62646
SunYan <garrysun9993@gmail.com>
2019-07-17 18:06:18 +0800

    Coding 1.3

--- 1.3.py
+++ 1.3.py
@@ -3,3 +3,24 @@
 # An extra copy of the array is not.
 # FOLLOW UP
 # Write the test cases for this method.
+
+
+# Simple version
+def removeDuplicates(string: str) -> str:
+    ans = ""
+    for i in range(len(string)):
+        isDuplicate = False
+        for c in ans:
+            if c == string[i]:
+                isDuplicate = True
+                break
+        if not isDuplicate:
+            ans += string[i]
+    return ans
+
+
+if __name__ == '__main__':
+    s1 = "aaaaaa"
+    s2 = "abc"
+    s3 = "aaaaacccccc"
+    print(removeDuplicates(s1), removeDuplicates(s2), removeDuplicates(s3))
